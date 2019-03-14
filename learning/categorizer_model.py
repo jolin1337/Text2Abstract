c8de7832c26cd6c2ec4b1312986c1f910221216d
Johannes Lindén <johannes.93.1337@gmail.com>
2019-03-14 15:21:59 +0100

    SLDD-849: Using add word2vec-model

---
+++ learning/categorizer_model.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+from logger import log
+import keras
+import numpy as np
+import pandas
+import pickle
+from learning.utils import striphtml, split_train_validation_data, f1_score, offset_binary_accuracy
+from learning.word2vec_model import Word2vecModel
+from learning.doc2vec_model import Doc2vecModel
+
+pandas.set_option('display.expand_frame_repr', False)
+
+
+class Categorizer(object):
+    def __init__(self, vec_model, model_file=None):
+        self.vec_model = vec_model
+        if model_file is not None:
+            self.load_model(model_file)
+            log("Loaded model with %s categories" % ",".join(self.categories))
+        else:
+            self.model = None
+        self.timestep = 20
+        self.epochs = 1
+
+    def preprocess_text(self, texts):
+        print("Preprocess text")
+        document_texts = [keras.preprocessing.text.text_to_word_sequence(striphtml(text))
+                      for text in texts]
+        for i, doc in enumerate(document_texts):
+            if isinstance(self.vec_model, Doc2vecModel):
+                vectors = np.zeros([self.timestep, self.vec_model.vector_size()])
+                for i in range(self.timestep):
+                    vec = self.vec_model.infer_vector(doc[i*self.timestep:(i+1)*self.timestep])
+                    pad_length = max(0, self.timestep - len(doc))
+                    if pad_length > 0:
+                        pad_vector = np.zeros([pad_length,self.vec_model.vector_size()])
+                        vec = np.append(vec, pad_vector, axis=0)
+                    print(vec.shape)
+                    vectors[i] = vec
+                yield vectors
+            elif isinstance(self.vec_model, Word2vecModel):
+                if len(doc) == 0:
+                    print("Length of doc", len(doc), i, len(document_texts), texts[i])
+                vec = self.vec_model.infer_vector(doc)
+                pad_length = max(0, self.timestep - len(doc))
+                if pad_length > 0:
+                    pad_vector = np.zeros([pad_length,self.vec_model.vector_size()])
+                    if not vec.tolist():
+                        print("Pad vector empty!", self.vec_model.vector_size())
+                    vec = np.append(vec, pad_vector, axis=0)
+                yield vec
+
+        #timestep_words = [[self.vec_model.infer_vector(seq[i*self.timestep:(i+1)*self.timestep])
+        #                   for i in range(self.timestep)]
+        #                   for seq in text_words]
+        #pad_words = np.array([np.append(seq[0:self.timestep],
+        #                                np.zeros([max(0, self.timestep - len(seq)),*self.vec_model.vector_shape()]),
+        #                                axis=0)
+        #             for seq in timestep_words])
+        #print(pad_words.shape, set([p.shape for p in pad_words]))
+        #return pad_words
+
+    def categorize_text(self, text):
+        if self.model == None:
+            raise UnknownModelException()
+        processed_text = np.array(self.preprocess_text(text))
+        probas = self.model.predict([processed_text])
+        return [{
+            c: float(p)
+            for c, p in zip(self.categories, list(proba))
+        } for proba in probas]
+
+    def construct_model(self, categories):
+        raise "Not implemented"
+
+    def train_categorizer(self, x_data, y_data, split_train_val=0.9, **model_args):
+      self.categories = list(set(c for y in y_data for c in y))
+      y_data = [[self.categories.index(c) for c in y] for y in y_data]
+      y_data_one_hot = encode_n_hot_vectors(y_data)
+      x_data_processed = list(self.preprocess_text(x_data))
+      x_train, y_train, x_val, y_val = split_train_validation_data(split_train_val, x_data_processed, y_data_one_hot)
+
+      log("Done preprocessing data")
+      model = self.construct_model(self.categories)
+      model.compile(loss='categorical_crossentropy',
+                    optimizer='rmsprop',
+                    metrics=['accuracy', f1_score])
+      log("Labels: ", self.categories)
+      model.fit([x_train], [y_train], validation_data=([x_val], [y_val]),
+                 **{'epochs': self.epochs, **model_args})
+      return model
+
+    def evaluate_categorizer(self, x_data, y_data):
+        if not self.model:
+            raise Exception("No model defined do evaluate")
+        self.model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy', f1_score])
+        y_data = [[self.categories.index(c) for c in y] for y in y_data]
+        y_data_one_hot = np.array(encode_n_hot_vectors(y_data, self.categories))
+        x_data_processed = np.array(self.preprocess_text(x_data))
+        evaluation = self.model.evaluate([x_data_processed], [y_data_one_hot])
+        log({name: val for name, val in zip(self.model.metrics_names, evaluation)})
+
+        # Predict all values of validation data
+        prediction = self.model.predict([x_data_processed])
+        true_positives = len([x
+                               for index, x in enumerate(prediction)
+                               if x.argmax() in [i for i, y in enumerate(y_data_one_hot[index]) if y == 1.0]])
+        total_predictions = len(prediction)
+        log("Accuracy %f" % (true_positives / total_predictions,))
+
+        # Compute confusion matrix of validation data
+        predicted_values = [[1 if np.argmax(x) == i else 0 for i, v in enumerate(x)] for x in prediction]
+        true_values = [[1 if np.argmax(x) == i else 0 for i, v in enumerate(x)] for x in y_data_one_hot]
+        confusion_matrix = np.zeros((len(true_values[0]), len(true_values[0])))
+        for i in range(len(confusion_matrix)):
+            for j in range(len(confusion_matrix)):
+                confusion_matrix[i][j] = len([1 for k, y in enumerate(true_values) if np.argmax(predicted_values[k]) == i and y[j] == 1])
+        pandas.set_option('display.expand_frame_repr', False)
+        cf = pandas.DataFrame(confusion_matrix, index=self.categories, columns=self.categories)
+        cf.loc['sum'] = cf.sum(numeric_only=True, axis=0)
+        cf['sum'] = cf.sum(numeric_only=True, axis=1)
+        log(cf)
+
+    def load_model(self, path):
+        obj = pickle.load(open(path + '.pkl', 'rb'))
+        self.categories = obj['categories']
+        self.vec_model = obj['vec_model']
+        self.model = keras.models.model_from_json(obj['model_json'])
+        self.model.load_weights(path + '.model.h5')
+        self.model._make_predict_function()
+        self.epochs = obj['epochs']
+        self.n_layers = obj['n_layers']
+        self.timestep = obj['timestep']
+
+    def save_model(self, path):
+        obj = {
+            'categories': self.categories,
+            'vec_model': self.vec_model,
+            'model_json': self.model.to_json(),
+            'epochs': self.epochs,
+            'n_layers': self.n_layers,
+            'timestep': self.timestep
+        }
+        self.model.save_weights(path + '.model.h5')
+        pickle.dump(obj, open(path + '.pkl', 'wb'))
+
+
+def encode_n_hot_vectors(y_data, categories=None):
+    categories = categories or list(set(c for y in y_data for c in y))
+    y_data_one_hot = np.zeros((len(y_data), len(categories)))
+    for i, y in enumerate(y_data_one_hot):
+        y[y_data[i]] = 1
+    return y_data_one_hot
+
+
+
---
+++ learning/doc2vec_model.py
@@ -0,0 +1,77 @@
+import gensim
+import random
+import re
+import config
+
+
+def loadStopWords(filepath):
+  stopwords = [line.strip() for line in open(filepath,'r',encoding="utf-8").readlines()]
+  return stopwords
+
+
+def removeStopWords(sentence):
+  stopwords = loadStopWords(config.data['path'] + config.data['stop_words'])
+  output = ''
+  sentence_seg = sentence.split(' ') # jieba.cut(sentence)
+  for word in sentence_seg:
+    if word not in stopwords and word != "":
+      output += word
+      output += " "
+  return output
+
+
+class Doc2vecModel(object):
+    def __init__(self, model=None, deterministic=False):
+        self.model = None
+        self.deterministic = deterministic
+        if deterministic:
+            random.seed(0)
+        if model is not None:
+            self.load_model(model)
+
+    def load_model(self, model):
+        self.model = gensim.models.Doc2Vec.load(model)
+        return self
+
+    def save_model(self, model):
+        self.model.save(model)
+        return self
+
+    def train(self, data, c_data=None, epochs=10, vector_size=300, alpha=0.025):
+        clean_text_re = '[，。：？“”！、（）《》’!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]+'
+        def to_labeled_sentence(data, c_data):
+            for i, x in enumerate(data):
+                x_only_words = re.sub(clean_text_re, ' ', x.strip())
+                x_no_stop_words = removeStopWords(x_only_words)
+                if c_data:
+                    yield gensim.models.doc2vec.LabeledSentence(x_no_stop_words, c_data[i])
+                else:
+                    yield gensim.models.doc2vec.LabeledSentence(x_no_stop_words, [i])
+        self.model = gensim.models.Doc2Vec(vector_size=vector_size,
+                                           alpha=alpha,
+                                           min_alpha=alpha,
+                                           min_count=1,
+                                           dm=1)
+        self.model.build_vocab(to_labeled_sentence(data, c_data))
+        for epoch in range(epochs):
+            self.model.train(to_labeled_sentence(data, c_data),
+                             total_examples=self.model.corpus_count,
+                             epochs=self.model.iter)
+            # decrease the learning rate
+            self.model.alpha -= 0.002
+            # fix the learning rate, no decay
+            self.model.min_alpha = self.model.alpha
+        return self
+
+    def infer_vector(self, words, seed=0):
+        if self.deterministic:
+            self.model.random.seed(seed) # Make the results deterministic for each prediction (obs: not over several runs)
+        return self.model.infer_vector(words)
+
+    def vector_size(self):
+        if self.model:
+            return self.model.vector_size
+        return None
+    def vector_shape(self):
+        if self.model:
+            return (self.model.vector_size,)
